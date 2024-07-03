a1759a8ee37e8cc9e9004d01038519232efec5f3
R_HYTE <polycarpwathuta39@gmail.com>
2024-07-03 21:08:48 +0300

    feat: Implemented DBClient class for MongoDB connection and data operations.

---
+++ utils/db.js
@@ -0,0 +1,49 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.isClientConnected = false;
+
+    this.client.connect((err) => {
+      if (err) {
+        console.error('MongoDB connection error:', err);
+        this.isClientConnected = false;
+      } else {
+        this.isClientConnected = true;
+      }
+    });
+  }
+
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async nbUsers() {
+    if (!this.isAlive()) {
+      throw new Error('MongoDB connection error');
+    }
+
+    const usersCollection = this.client.db().collection('users');
+    const count = await usersCollection.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    if (!this.isAlive()) {
+      throw new Error('MongoDB connection error');
+    }
+
+    const filesCollection = this.client.db().collection('files');
+    const count = await filesCollection.countDocuments();
+    return count;
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
